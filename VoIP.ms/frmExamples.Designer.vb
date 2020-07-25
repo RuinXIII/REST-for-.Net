9363d6478b5b1dd632efb667e590902d98b50458
Ruiyn <lavosx@gmail.com>
2020-07-25 03:18:34 -0500

    Initial Commit

---
+++ VoIP.ms/frmExamples.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmExamples
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGetIPAddress = New System.Windows.Forms.Button()
+        Me.btnGetBalance = New System.Windows.Forms.Button()
+        Me.btnGetServers = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGetIPAddress
+        '
+        Me.btnGetIPAddress.Location = New System.Drawing.Point(34, 23)
+        Me.btnGetIPAddress.Name = "btnGetIPAddress"
+        Me.btnGetIPAddress.Size = New System.Drawing.Size(111, 23)
+        Me.btnGetIPAddress.TabIndex = 0
+        Me.btnGetIPAddress.Text = "Get IP Address"
+        Me.btnGetIPAddress.UseVisualStyleBackColor = True
+        '
+        'btnGetBalance
+        '
+        Me.btnGetBalance.Location = New System.Drawing.Point(151, 23)
+        Me.btnGetBalance.Name = "btnGetBalance"
+        Me.btnGetBalance.Size = New System.Drawing.Size(111, 23)
+        Me.btnGetBalance.TabIndex = 1
+        Me.btnGetBalance.Text = "Get Balance"
+        Me.btnGetBalance.UseVisualStyleBackColor = True
+        '
+        'btnGetServers
+        '
+        Me.btnGetServers.Location = New System.Drawing.Point(98, 61)
+        Me.btnGetServers.Name = "btnGetServers"
+        Me.btnGetServers.Size = New System.Drawing.Size(111, 23)
+        Me.btnGetServers.TabIndex = 2
+        Me.btnGetServers.Text = "Get CAN Servers"
+        Me.btnGetServers.UseVisualStyleBackColor = True
+        '
+        'frmExamples
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(306, 110)
+        Me.Controls.Add(Me.btnGetServers)
+        Me.Controls.Add(Me.btnGetBalance)
+        Me.Controls.Add(Me.btnGetIPAddress)
+        Me.Name = "frmExamples"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Examples REST for .Net"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGetIPAddress As Button
+    Friend WithEvents btnGetBalance As Button
+    Friend WithEvents btnGetServers As Button
+End Class
---
+++ VoIP.ms/frmExamples.vb
@@ -0,0 +1,89 @@
+﻿' This project uses Newtonsoft.json to parse the JSON response from the API
+' To Install it go to "Tools > NuGet Package Manager > Package Manager Console"
+' Run the following command in the console: install-package Newtonsoft.json
+
+Imports System.Net.Http
+Imports Newtonsoft.Json.Linq
+
+Public Class frmExamples
+
+    Dim api_url As String = "https://voip.ms/api/v1/rest.php"
+    Dim strUsername As String = "lavosx@gmail.com"
+    Dim strPassword As String = "Inpachi00"
+
+    Private Sub btnGetIPAddress_Click(sender As Object, e As EventArgs) Handles btnGetIPAddress.Click
+        Dim client As New HttpClient
+        Dim content As New MultipartFormDataContent
+
+        Dim api_username As New StringContent(strUsername)
+        Dim api_password As New StringContent(strPassword)
+        Dim method As New StringContent("getIP")
+
+        content.Add(api_username, "api_username")
+        content.Add(api_password, "api_password")
+        content.Add(method, "method")
+
+        Dim response = client.PostAsync(api_url, content).Result
+        Dim jsonResponse = response.Content.ReadAsStringAsync()
+        Dim jsonObject As JObject = JObject.Parse(jsonResponse.Result.ToString())
+
+        If jsonObject("status") = "success" Then
+            MessageBox.Show(jsonObject("ip"))
+        Else
+            MessageBox.Show(jsonObject("status"))
+        End If
+    End Sub
+
+    Private Sub btnGetBalance_Click(sender As Object, e As EventArgs) Handles btnGetBalance.Click
+        Dim client As New HttpClient
+        Dim content As New MultipartFormDataContent
+
+        Dim api_username As New StringContent(strUsername)
+        Dim api_password As New StringContent(strPassword)
+        Dim method As New StringContent("getBalance")
+
+        content.Add(api_username, "api_username")
+        content.Add(api_password, "api_password")
+        content.Add(method, "method")
+
+        Dim response = client.PostAsync(api_url, content).Result
+        Dim jsonResponse = response.Content.ReadAsStringAsync()
+        Dim jsonObject As JObject = JObject.Parse(jsonResponse.Result.ToString())
+
+        If jsonObject("status") = "success" Then
+            MessageBox.Show(jsonObject("balance")("current_balance"))
+        Else
+            MessageBox.Show(jsonObject("status"))
+        End If
+    End Sub
+
+    Private Sub btnGetServers_Click(sender As Object, e As EventArgs) Handles btnGetServers.Click
+        Dim client As New HttpClient
+        Dim content As New MultipartFormDataContent
+
+        Dim api_username As New StringContent(strUsername)
+        Dim api_password As New StringContent(strPassword)
+        Dim method As New StringContent("getServersInfo")
+
+        content.Add(api_username, "api_username")
+        content.Add(api_password, "api_password")
+        content.Add(method, "method")
+
+        Dim response = client.PostAsync(api_url, content).Result
+        Dim jsonResponse = response.Content.ReadAsStringAsync()
+        Dim jsonObject As JObject = JObject.Parse(jsonResponse.Result.ToString())
+
+        Dim strOutput As String = ""
+
+        If jsonObject("status") = "success" Then
+            For Each Row In jsonObject("servers")
+                If Row("server_country") = "CAN" Then
+                    strOutput += Row("server_hostname").ToString() & vbCrLf
+                End If
+            Next
+            MessageBox.Show(strOutput)
+        Else
+            MessageBox.Show(jsonObject("status"))
+        End If
+    End Sub
+End Class
